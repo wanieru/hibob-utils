55fd72013bdd1f75c7529d62cfd29a6469b3cf65
Daniel Hansen <dhansen@fundayfactory.com>
2022-11-02 09:24:42 +0100

    1.1.7

--- hibob.meta.js
+++ hibob.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        HiBob Time Utilities
-// @version     1.1.6
+// @version     1.1.7
 // @author      Funday Factory
 // @description Utilities to make it easier to log time in HiBob
 // @match       https://app.hibob.com/*
